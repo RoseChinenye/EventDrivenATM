107629a7bfe2b3776ae986bfab3aa524d3276506
Chinenye <c.okeke@genesystechhub.com>
2022-12-05 18:03:11 +0100

    Add project files.

---
+++ EventDrivenATM/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EventDrivenATM;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 1. English\n 2. Igbo\n ");
+            string? option = Console.ReadLine();
+
+            if (int.TryParse(option, out int language))
+            {
+                switch (language)
+                {
+                    case 1:
+                        atmApp.getEnglish();
+                        break;
+                    case 2:
+                        atmApp.getIgbo();
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter 1 or 2");
+                        Program.Main(args);
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your input is not a numerical value! Enter 1 or 2");
+                Program.Main(args);
+            }
+
+
+        }
+    }
+
---
+++ EventDrivenATM/TransactionsDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace EventDrivenATM; 
+    internal class TransactionsDetails 
+    {
+            public double Amount { get; }
+            public DateTime Date { get; }
+
+            public TransactionsDetails(double amount)
+            {
+                Amount = amount;
+
+            }
+    }
+
